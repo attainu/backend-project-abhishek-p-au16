79b91033a344553f332d09ead0df13113b23eae4
sarvesh-mishra-au16 <misrasarvesh16@gmail.com>
2021-05-27 16:13:05 +0530

    added tables to list products  in admin pannel

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  let products=[{
+    no:1,
+    name:"Dabur Honey",
+    catogary:"honey",
+    discription:" 100% Pure World's No.1 Honey Brand with No Sugar Adulteration - 1kg (Get 20% Extra)",
+    image:"https://images-na.ssl-images-amazon.com/images/I/81hghkkAaSL._SL1500_.jpg"
+  },
+  { no:'2',
+    name:'Surf Excel',
+    catogary:"detergent",
+    discription:"Surf Excel Easy Wash Detergent Powder, Superfine Powder That Dissolves Easily And Removes Tough Stains, 1.5 Kg",
+    image:"https://images-na.ssl-images-amazon.com/images/I/61JjrPGRnTL._SX679_.jpg"
+  },{
+    no:3,
+    name:"Dove Hair Fall Rescue",
+    catogary:"shampoo",
+    discription:"Dove Hair Fall Rescue Conditioner For Weak, Frizzy Hair, Reduces Hair Fall and Makes Hair Strong & Frizz protected, 180 ml",
+    image:"https://images-na.ssl-images-amazon.com/images/I/51sdu-khPYL._SL1000_.jpg"
+  },
+  {
+    no:4,
+    name:"nutella",
+    catagory:"chocolate",
+    discription:"Nutella Hazelnut Spread with Cocoa (Labels may vary), 350g",
+    image:"https://images-na.ssl-images-amazon.com/images/I/714A58g3xWS._SL1500_.jpg"
+  }]
+  res.render('admin/view-products',{products,admin:true});
 });
 
 module.exports = router;
